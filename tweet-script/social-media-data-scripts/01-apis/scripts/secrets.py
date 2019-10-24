4ed6545c400d43b86c8a19c6546132a287bdfe4d
Meshal Al-Sultan <51976236+meshalalsultan@users.noreply.github.com>
2019-10-24 04:27:16 +0300

    Add files via upload

---
+++ tweet-script/social-media-data-scripts/01-apis/scripts/secrets.py
@@ -0,0 +1,16 @@
+# Twitter API credentials, you can get them here https://apps.twitter.com/
+
+TWITTER_C_KEY = "CCKrzZIJaEjwXuEM3k5Hjg4st"
+TWITTER_C_SECRET = "4aqA285brrvx73FYj9gDm7PdM16CJKYmGb4kU1lzhVQY5a9U18"
+TWITTER_A_KEY = "1184086168608088064-dTXOKpcuRAGXRLfLtw2LPC6LvjdYq9"
+TWITTER_A_SECRET = "4J1ziz9igxYHzVHKsEHC15241Uap60ZNyQ7GjftpgFMVi"
+
+# Facebook API credentials
+FACEBOOK_APP_ID = ""
+FACEBOOK_APP_SECRET = ""
+
+# YouTube credentials
+YOUTUBE_API_KEY = ""
+
+# Giphy credentials
+GIPHY_API_KEY = ""
